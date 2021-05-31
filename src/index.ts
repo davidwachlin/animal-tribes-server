f0220d4c6d6381b01bf051bce87667503af124e7
David Wachlin <davidwachlin@gmail.com>
2021-05-31 18:21:09 -0400

    set up

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import * as express from 'express'
+import * as cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+
+app.listen(3333, () => {
+  console.log(`now listening for requests on port 3333`)
+})
+
+export default app
